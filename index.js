44d89c02587556f37c0fca28fb2fb7367a5e4d20
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-10-23 12:27:01 -0700

    recursive function added

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 
 function sum(fromN, toN) {
+  if (fromN === toN) {
+    return fromN;
+  } else {
+    return fromN + sum(fromN + 1, toN);
+  }
+
+
+
   // Sum all the values from fromN up to toN
+  // for (let i = fromN; i < toN; i++) {
+  //   fromN = fromN + 1 + i;
+  // }
+
 }
 
 module.exports = sum;
